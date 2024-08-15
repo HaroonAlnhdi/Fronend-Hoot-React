03311d25924033eb308039108c4a7f9f95fe4454
Denis S Dujota <denisdujota@gmail.com>
2024-08-15 09:54:33 +0300

    add base comps for hoot list

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm'
+import HootList from './components/HootList/HootList';
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser());
@@ -22,9 +23,14 @@ const App = () => {
       <NavBar user={user} handleSignout={handleSignout}/>
       <Routes>
         { user ? (
-          <Route path="/" element={<Dashboard user={user} />} />
+          // Protected Routes:
+          <>
+            <Route path="/" element={<Dashboard user={user} />} />
+            <Route path="/hoots" element={<HootList />} />
+          </>
         ) : (
-          <Route path="/" element={<Landing />} />
+            // Public Route:
+            <Route path="/" element={<Landing />} />
         )}
         <Route path="/signup" element={<SignupForm setUser={setUser}/>} />
         <Route path="/signin" element={<SigninForm setUser={setUser}/>} />
